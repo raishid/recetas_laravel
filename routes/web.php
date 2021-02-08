27f437d35de431654156847a9144804fd5203b36
fededav <67575679+raishid@users.noreply.github.com>
2021-02-07 22:48:05 -0400

    continuar con el video
    continuar con el video 9 controller y views de la carpeta vistas y controlador

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +12,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/recetas', 'recetasController@index');
